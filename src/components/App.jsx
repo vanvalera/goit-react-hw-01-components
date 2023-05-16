a62b59269eaa4061310d6df971c7db860348d865
vanvalera <vmarton777@gmail.com>
2023-05-16 21:57:37 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import Profile from "./Profile/Profile";
+import user from './Profile/user.json'
 export const App = () => {
   return (
     <div
@@ -7,10 +9,17 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        background: 'grey'
       }}
-    >
-      React homework template
+    > 
+      <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
     </div>
   );
 };
